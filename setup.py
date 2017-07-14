c0b53c44739e4484fe76f09cd2bc1f4d0cf7ac31
John Hwang <hwangjt@Johns-MacBook-Pro-2015.local>
2017-07-13 21:34:07 -0700

    Minor change

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from distutils.core import setup, Extension
 from Cython.Build import cythonize
+import numpy as np
 
 
 sources = [
@@ -9,5 +10,6 @@ sources = [
 
 setup(ext_modules = cythonize(Extension(
     "fem2d", sources=sources,
-    language="c++", extra_compile_args=['-std=c++11']
+    language="c++", extra_compile_args=['-std=c++11'],
+    include_dirs=[np.get_include()]
 )))
